c70f6b2473d9d3111c7a7cfe1744a36eb796e14d
Mohamed Riyad Bouzani <mriyadbouzani@gmail.com>
2023-09-17 02:11:15 +0100

    Fixed coin card layout

--- src/components/CoinCard.tsx
+++ src/components/CoinCard.tsx
@@ -1,5 +1,5 @@
 import { Card, Typography } from "@mui/joy";
-import { Coin, CoinDetail } from "../model/coin";
+import { CoinDetail } from "../model/coin";
 import { Votes } from "../model/vote";
 import { ArrowDown, ArrowUp } from "react-feather";
 
@@ -10,22 +10,44 @@ interface CoinCardProps {
 
 const CoinCard = ({ coin, vote }: CoinCardProps) => {
 	return (
-		<Card sx={{ width: "80%", height:200}}>
+		<Card sx={{ width: "80%", height: "max-content" }}>
 			<div className="flex justify-between">
 				<div className="flex flex-col">
 					<div className="flex items-start gap-2">
 						<img src={coin.image.small}></img>
-						<div>
-							<div className="flex gap-2">
-								<Typography level="h2">{coin.name}</Typography>
-								<Typography level="h2" color="neutral">
-									{coin.symbol.toUpperCase()}
+						<div className="flex flex-col gap-4">
+							<div className="flex flex-col">
+								<div className="flex gap-2">
+									<Typography level="h2">
+										{coin.name}
+									</Typography>
+									<Typography level="h2" color="neutral">
+										{coin.symbol.toUpperCase()}
+									</Typography>
+								</div>
+								<div>
+									<Typography level="h2">
+										{coin.market_data.current_price.usd}{" "}
+										<span className="text-neutral-500">
+											$
+										</span>
+									</Typography>
+								</div>
+							</div>
+							<div>
+								<Typography level="body-lg" color="neutral">
+									Market Cap
+								</Typography>
+								<Typography level="body-sm" color="neutral">
+									{coin.market_data.market_cap.usd} $
 								</Typography>
 							</div>
 							<div>
-								<Typography level="h2">
-									{coin.market_data.current_price.usd}{" "}
-									<span className="text-neutral-500">$</span>
+								<Typography level="body-lg" color="neutral">
+									Market Cap Rank
+								</Typography>
+								<Typography level="body-sm" color="neutral">
+									#{coin.market_data.market_cap_rank}
 								</Typography>
 							</div>
 						</div>
@@ -47,9 +69,24 @@ const CoinCard = ({ coin, vote }: CoinCardProps) => {
 							<ArrowDown />
 						</div>
 					</div>
-                    <Typography level="h3">Total Votes</Typography>
+					<Typography level="h3">Total Votes</Typography>
 					<div>
 						<Typography level="h2">{vote.totalVotes}</Typography>
+					</div>
+					<Typography level="h3">Average vote %</Typography>
+					<div className="flex items-start gap-4">
+						<div className="flex items-center">
+							<Typography level="h2" textColor={"rgb(22 163 74)"}>
+								{vote.upVotesPercentageAvg}
+							</Typography>
+							<ArrowUp />
+						</div>
+						<div className="flex items-center">
+							<Typography level="h2" textColor={"rgb(239 68 68)"}>
+								{vote.downVotesPercentageAvg}
+							</Typography>
+							<ArrowDown />
+						</div>
 					</div>
 				</div>
 			</div>
--- src/model/vote.ts
+++ src/model/vote.ts
@@ -14,4 +14,6 @@ export interface Votes{
     upVotes: number;
     downVotes: number;
     totalVotes: number;
+    upVotesPercentageAvg: number;
+    downVotesPercentageAvg: number;
 }
