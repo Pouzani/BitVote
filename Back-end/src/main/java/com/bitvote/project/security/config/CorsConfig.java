bb6b2281d1c27dc2c1409c484b4e728bd37a11e7
Mohamed Riyad Bouzani <mriyadbouzani@gmail.com>
2023-12-19 10:56:07 +0100

    test v2

--- Back-end/src/main/java/com/bitvote/project/security/config/CorsConfig.java
+++ Back-end/src/main/java/com/bitvote/project/security/config/CorsConfig.java
@@ -10,18 +10,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 public class CorsConfig implements CorsConfigurationSource {
     @Value("${application.security.cors.allowed-origins}")
     private String origin;
-    /**
-     * The getCorsConfiguration function is used to configure the CORS policy for the resource.
-     * The method returns null for the defined origin, headers, and methods in order to allow any origin access to the API.
-     * This configuration is suitable only for development environments where you want quick and easy access from a browser.
 
-     *
-     * @param HttpServletRequest request Get the origin of the request
-     *
-     * @return A corsconfiguration object
-     *
-     * @docauthor Trelent
-     */
     @Override
     public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
         CorsConfiguration config = new CorsConfiguration();
